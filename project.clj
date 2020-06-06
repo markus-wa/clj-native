7bdea8fec564956699cffe445f2c6fcf4b3b4eb3
Markus Walther <m.walther97@gmail.com>
2020-06-06 14:17:43 +0100

    upgrade clojure & jna versions

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-native "0.9.5"
+(defproject clj-native "0.9.6-SNAPSHOT"
   :description "Simplify usage of native libs from Clojure. Uses JNA."
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [net.java.dev.jna/jna "4.1.0"]])
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [net.java.dev.jna/jna "4.5.0"]])
